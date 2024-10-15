04b59d0b9f712b83f0b1f00c409a8ad27dcf1a6b
libresoul <dragoofficial991@gmail.com>
2024-10-16 01:02:13 +0530

    add client country fetching again

--- functions/all.js
+++ functions/all.js
@@ -1,8 +1,12 @@
-export function onRequest(context) {
+export async function onRequest(context) {
     const clientIP = context.request.headers.get("CF-Connecting-IP");
-    const clientCountry = context.request.headers.get("CF-IPCountry");
+    const alphacode = context.request.headers.get("CF-IPCountry");
+    const response = await fetch(`https://restcountries.com/v3.1/alpha/${alphacode}?fields=name`);
+    const cdetails = await response.json();
+    
     return new Response(
 `IP Address: ${clientIP}
-Country: ${clientCountry}`
+Country: ${JSON.stringify(cdetails.name.common)}`
     );
 }
+
