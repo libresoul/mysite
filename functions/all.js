641e7913221effa3668f8f8cc3a709f20ede45c1
libresoul <dragoofficial991@gmail.com>
2024-10-15 23:58:57 +0530

    fix response formatting

--- functions/all.js
+++ functions/all.js
@@ -1,6 +1,8 @@
 export function onRequest(context) {
     const clientIP = context.request.headers.get("CF-Connecting-IP");
     const clientCountry = context.request.headers.get("CF-IPCountry");
-    return new Response(`IP Address: ${clientIP}
-        Country: ${clientCountry}`);
+    return new Response(
+`IP Address: ${clientIP}
+Country: ${clientCountry}`
+    );
 }
