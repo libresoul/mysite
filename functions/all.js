f5ba46f96b0a339d3790676b3d1db0d52ffcf041
libresoul <dragoofficial991@gmail.com>
2024-10-15 23:48:09 +0530

    add function for client ip and country

---
+++ functions/all.js
@@ -0,0 +1,6 @@
+export function onRequest(context) {
+    const clientIP = context.request.headers.get("CF-Connecting-IP");
+    const clientCountry = context.request.headers.get("CF-IPCountry");
+    return new Response(`IP Address: ${clientIP}
+        Country: ${clientCountry}`);
+}
