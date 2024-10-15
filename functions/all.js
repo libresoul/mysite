90074e55ac86c1a2a5b5bc104fd5cc131ce6c4f5
libresoul <dragoofficial991@gmail.com>
2024-10-16 00:52:37 +0530

    Revert "add client country fetching"
    This reverts commit f942ae05f9cd1d3387c3d287657346c025b0223f.

--- functions/all.js
+++ functions/all.js
@@ -1,11 +1,8 @@
-export async function onRequest(context) {
+export function onRequest(context) {
     const clientIP = context.request.headers.get("CF-Connecting-IP");
-    const alphacode = context.request.headers.get("CF-IPCountry");
-    const data = await fetch(`https://restcountries.com/v3.1/alpha/${alphacode}?fields=name`);
-    const cdetails = data.json();
-    
+    const clientCountry = context.request.headers.get("CF-IPCountry");
     return new Response(
 `IP Address: ${clientIP}
-Country: ${JSON.stringify(cdetails.name.common)}`
+Country: ${clientCountry}`
     );
 }
