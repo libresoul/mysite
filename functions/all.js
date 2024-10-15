090b0b23c8c5092f053b9bfd54827b18234f0258
libresoul <dragoofficial991@gmail.com>
2024-10-15 23:57:24 +0530

    Revert "format all.js"
    This reverts commit e0035f25fec39257b5c8bda47267a0a63ac8f0c1.

--- functions/all.js
+++ functions/all.js
@@ -1,9 +1,6 @@
 export function onRequest(context) {
     const clientIP = context.request.headers.get("CF-Connecting-IP");
     const clientCountry = context.request.headers.get("CF-IPCountry");
-    // left align all the responses
-    return new Response(
-    `IP Address: ${clientIP}\n
-    Country: ${clientCountry}`
-    );
+    return new Response(`IP Address: ${clientIP}
+        Country: ${clientCountry}`);
 }
