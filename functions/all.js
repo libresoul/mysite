e0035f25fec39257b5c8bda47267a0a63ac8f0c1
libresoul <dragoofficial991@gmail.com>
2024-10-15 23:52:32 +0530

    format all.js

--- functions/all.js
+++ functions/all.js
@@ -1,6 +1,9 @@
 export function onRequest(context) {
     const clientIP = context.request.headers.get("CF-Connecting-IP");
     const clientCountry = context.request.headers.get("CF-IPCountry");
-    return new Response(`IP Address: ${clientIP}
-        Country: ${clientCountry}`);
+    // left align all the responses
+    return new Response(
+    `IP Address: ${clientIP}\n
+    Country: ${clientCountry}`
+    );
 }
