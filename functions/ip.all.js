3a542f8b355b308e2ab49adbe4b0c56ae3b08959
Damindu Dhananjitha <contact@damindu.me>
2024-11-06 21:51:42 +0530

    feat: add ip and common name pages function (#3)
    * add function for client ip and country
    
    * format all.js
    
    * Revert "format all.js"
    
    This reverts commit e0035f25fec39257b5c8bda47267a0a63ac8f0c1.
    
    * fix response formatting
    
    * add client country fetching
    
    * Revert "add client country fetching"
    
    This reverts commit f942ae05f9cd1d3387c3d287657346c025b0223f.
    
    * add client country fetching again
    
    * remove stringifying country
    
    * rename ip.js to all.js

---
+++ functions/ip.all.js
@@ -0,0 +1,12 @@
+export async function onRequest(context) {
+    const clientIP = context.request.headers.get("CF-Connecting-IP");
+    const alphacode = context.request.headers.get("CF-IPCountry");
+    const response = await fetch(`https://restcountries.com/v3.1/alpha/${alphacode}?fields=name`);
+    const cdetails = await response.json();
+    
+    return new Response(
+`IP Address: ${clientIP}
+Country: ${cdetails.name.common}`
+    );
+}
+
